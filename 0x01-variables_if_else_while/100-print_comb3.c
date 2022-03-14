4ce3bd9305c534d5561b10bbda51e81d45535b93
davidadediji <davidadediji@gmail.com>
2022-03-14 12:13:21 +0000

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,14 +8,13 @@ int main(void)
 	int i;
 
 	int j;
-	
+
 	for (i = '0'; i < '9'; i++)
 	{
 		for (j = i + 1; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
-			
 			if (i == '8' && j == '9')
 			continue;
 			putchar(',');
@@ -23,6 +22,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	
 	return (0);
 }
