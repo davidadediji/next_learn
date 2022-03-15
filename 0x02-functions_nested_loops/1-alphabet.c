e3c7483cda2511e9b1bb69fe471da7722de85c49
davidadediji <davidadediji@gmail.com>
2022-03-15 21:34:30 +0000

    task-1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - To print alphabet
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	int i = 'a';
+
+	while (i < 'z')
+	{
+		_putchar('i');
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
