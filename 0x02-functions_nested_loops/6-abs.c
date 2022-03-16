79aa073b7cb4a4610e5ec7771791c0bb5bd96e9a
davidadediji <davidadediji@gmail.com>
2022-03-16 18:40:15 +0000

    task-6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * int_abs - function to print the absolute value of an integer
- * Return - Always 0
+ *_abs - function to print the absolute value of an integer
+ * @c: c value to accept
+ * Return: `Always 0
  */
 int _abs(int c)
 {
