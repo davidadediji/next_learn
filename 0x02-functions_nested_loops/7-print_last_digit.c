02f1ddc7478c07c34409f90e7e65fb5176d5aa82
davidadediji <davidadediji@gmail.com>
2022-03-16 18:50:09 +0000

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,9 @@
+/**
+ * print_last_digit - function to print the last number
+ * @c: input accepted
+ * Return: returns the value of last digit
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
