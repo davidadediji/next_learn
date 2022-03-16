8327c4813ec1c7e5cdfcf2bb458dbe2a200f90e8
davidadediji <davidadediji@gmail.com>
2022-03-16 23:48:37 +0000

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,11 +9,11 @@ void print_to_98(int n)
 {
 	if (n >= 98)
 	{
-		for (n = n; n >= 98; n++)
+		for (n = n; n >= 98; n--)
 		{
 			if (n != 98)
 			{
-				printf("%d, ",n);
+				printf("%d, ", n);
 			}
 			else if (n == 98)
 			{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 		{
 			if (n != 98)
 			{
-				printf("%d, ",n);
+				printf("%d, ", n);
 			}
 			else if (n == 98)
 			{
