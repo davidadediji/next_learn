9625510057ec3b27bf023c94519b8cffaa6e256c
davidadediji <davidadediji@gmail.com>
2022-03-16 19:56:20 +0000

    task-11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - function to print
+ * @n:natural numbers
+ * Return: numbers from 0 to 98
+ */
+void print_to_98(int n)
+{
+	int n = 0;
+
+	while (n <= 98)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar(' ');
+	}
+}
