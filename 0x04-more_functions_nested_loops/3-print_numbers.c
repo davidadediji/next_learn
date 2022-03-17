5a2e913b4cbc858fcb97c3bd5bcec8c5e0817fff
davidadediji <davidadediji@gmail.com>
2022-03-17 23:14:24 +0000

    corrections

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * print_most_numbers - check for uppercase characters
+ * print_numbers - print numbers
  * Return: Always 0 or 1
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	int i;
 
 	while (i <= 9)
 	{
-		if (i == 2 || i == 4)
-		{
-			continue;
-		}
 		_putchar(i + '0');
 		_putchar('\n');
 		i++;
