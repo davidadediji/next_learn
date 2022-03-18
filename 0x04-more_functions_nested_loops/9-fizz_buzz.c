794bb0ac994cbe4a88a46a87b4a4cd34fcf209a4
davidadediji <davidadediji@gmail.com>
2022-03-18 14:48:57 +0000

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i <= 100; i++)
+	for (i = 1; i < 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
@@ -21,6 +21,10 @@ int main(void)
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
+		}
+		else if (i == 100)
+		{
+			printf("Buzz")
 		}
 		else
 		{
