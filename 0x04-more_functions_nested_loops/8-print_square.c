9fcb626b777e518de2314e5eb79e770e7def8822
Emma Paris <davidadediji@gmail.com>
2022-03-19 05:10:02 +0100

    debugging

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,20 +12,23 @@ void print_square(int size)
 		_putchar('\n');
 	}
 
-	for (i = 0; i < size; i++)
+	else
 	{
-		if (size == 0)
+		for (i = 0; i < size; i++)
 		{
+			if (size == 0)
+			{
+				_putchar('\n');
+			}
+			_putchar('#');
+
+			int j;
+
+			for (j = 0; j < size - 1; j++)
+			{
+				_putchar('#');
+			}
 			_putchar('\n');
 		}
-		_putchar('#');
-
-		int j;
-
-		for (j = 0; j < size - 1; j++)
-		{
-			_putchar('#');
-		}
-		_putchar('\n');
 	}
 }
