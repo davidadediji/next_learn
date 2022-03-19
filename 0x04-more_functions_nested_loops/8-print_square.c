3dcff29368fb91a9c7a0b40f1310023fe3924ad3
Emma Paris <davidadediji@gmail.com>
2022-03-19 05:12:02 +0100

    remove unused code

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,12 +15,8 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (size == 0)
-			{
-				_putchar('\n');
-			}
 			_putchar('#');
-
+			
 			int j;
 
 			for (j = 0; j < size - 1; j++)
