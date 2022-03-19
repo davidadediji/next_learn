5fc9e9641375216ba9a6a6a852b7a04d36751a0d
Emma Paris <davidadediji@gmail.com>
2022-03-19 04:53:26 +0100

    some corrections

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *print_diagonal - function to print a diagonal
+ * @n:variable required
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
