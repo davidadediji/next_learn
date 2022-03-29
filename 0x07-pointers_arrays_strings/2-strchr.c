394da354aa285036c66d9a2f02c0dcd159bb4df2
davidadediji <davidadediji@gmail.com>
2022-03-29 22:39:41 +0000

    task-2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,14 +9,17 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
+	if (s == NULL)
+		return (NULL);
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
 		{
-			return &s[i];
+			return (&s[i]);
 		}
 		if (s[i] == c)
-			return &s[i];
+			return (&s[i]);
 	}
 	return (0);
 }
