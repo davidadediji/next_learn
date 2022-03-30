71cdc7c20d0ef3f08890539f0a51da38ceb2e3d0
Emma Paris <davidadediji@gmail.com>
2022-03-31 00:48:21 +0100

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- *_strlen_recursion(char *s); - function to print a string in reverse
+ *_strlen_recursion(char *s) - function to print a string in reverse
  *@s:unsigned character
  *Return:int-type
  */
 int _strlen_recursion(char *s)
 {
 	int i;
+
 	i = 0;
 
 	if (*s != '\0')
 	{
 		_strlen_recursion(s + 1);
 		i++;
+		return (i);
 	}
-	return i;
 }
