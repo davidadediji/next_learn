d017f911b47c00aa5e7b80d63ce987080cf03b2a
Emma Paris <davidadediji@gmail.com>
2022-03-31 00:33:23 +0100

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- *_puts_recursion - function to print string followed by newline
+ *_print_rev_recursion - function to print a string in reverse
  *@s:unsigned character
  */
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 
 	if (*s != '\0')
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 	else
