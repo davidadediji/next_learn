ba0780b4a6841b4527a40aa140f355798891ae31
Emma Paris <davidadediji@gmail.com>
2022-03-31 03:25:25 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  *check_root - function to check the root of a number
+ *_sqrt_recursion - function to print a string in reverse
  *@a: interger value
  *@b: interger value
- *_sqrt_recursion - function to print a string in reverse
  *@n:unsigned character
  *Return:int-type
  *check for square root of a number
@@ -18,7 +18,8 @@ int check_root(int a, int b)
 }
 int _sqrt_recursion(int n)
 {
-
+	if (n == 1)
+		return (1);
 	if (n == 0)
 		return (0);
 	return (check_root(1, n));
