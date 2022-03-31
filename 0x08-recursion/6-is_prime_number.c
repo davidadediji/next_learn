f5da03f189251d99aefdd03bd853e5cf9fd2cc1e
Emma Paris <davidadediji@gmail.com>
2022-03-31 02:34:13 +0100

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_is_prime_number - function that returns 1 if interger value is a prime number and returns 0 when not.
+ *is_prime_number - function that returns 1 or 0 for prime values
  *@n:integer to test
  *Return:int
  */
 int is_prime_number(int n)
 {
-	if (n % n == 0 && n % 1 == 0 && n / 1 == n)
+	if (n % n == 0 && n % 1 == 0 && n / 1 == n && n > 1)
 	{
 		return (1);
 	}
