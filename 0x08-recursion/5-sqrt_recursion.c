900a8e13045380784dc81a8a0fae814eaa404943
Emma Paris <davidadediji@gmail.com>
2022-03-31 03:14:36 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,15 +3,20 @@
  *_sqrt_recursion - function to print a string in reverse
  *@n:unsigned character
  *Return:int-type
+ *check for square root of a number
  */
+int check_root(int a, int b)
+{
+	if (a + a == b)
+		return (a);
+	if(a * a > b)
+		return (-1);
+	return (check(a + 1, b));
+}
 int _sqrt_recursion(int n)
 {
-	unsigned int i;
 
 	if (n < 0 || n == 0 || n == 1)
 		return (0);
-	if (i * i == n)
-	{
-		return (i);
-	}
+	return (check(1, n));
 }
