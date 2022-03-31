6e9831bf0d6945a471cb7f5b24ab1ec18e7f359a
Emma Paris <davidadediji@gmail.com>
2022-03-31 02:30:54 +0100

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,17 @@
 #include "main.h"
 /**
- *_strlen_recursion(char *s) - function to print a string in reverse
- *@s:unsigned character
- *Return:int-type
+ *_is_prime_number - function that returns 1 if interger value is a prime number and returns 0 when not.
+ *@n:integer to test
+ *Return:int
  */
+int is_prime_number(int n)
+{
+	if (n % n == 0 && n % 1 == 0 && n / 1 == n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
