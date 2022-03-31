c152922ea561825c34c021b23a4a056cc1f42050
Emma Paris <davidadediji@gmail.com>
2022-03-31 01:41:38 +0100

    task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,5 +7,20 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int result = x;
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (y > 0)
+	{
+		result *= _pow_recursion(x, y - 1);
+	}
+	return (result);
 
 }
