b0bb76e44a451109c9da41ecf4737eb65b5b3668
Emma Paris <davidadediji@gmail.com>
2022-04-01 23:40:46 +0100

    fix task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main(int argc, char*argv[])
+/**
+ *main - function that accepts arguments
+ *@argc:argument count
+ *@argv: argument variables
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
+	return (0);
 }
