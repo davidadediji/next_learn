a20a8e67a9bdb5138a1df0175ab0d82d6fad1f05
Emma Paris <davidadediji@gmail.com>
2022-04-01 23:50:14 +0100

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - function that accepts arguments
+ *@argc:argument count
+ *@argv: argument variables
+ *Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	(void)argv;
+	return (0);
+}
